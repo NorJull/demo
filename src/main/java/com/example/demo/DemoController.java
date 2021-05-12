870a2f5962bf315953bff598894997bde96372e3
NorJull <narendavid10@hotmail.com>
2021-05-12 16:24:24 -0500

    First commit

---
+++ src/main/java/com/example/demo/DemoController.java
@@ -0,0 +1,18 @@
+package com.example.demo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class DemoController {
+
+    @GetMapping("/demo/{name}")
+    public String demo(@PathVariable String name){
+       return "Hello "+name+"! You are running a .war in tomcat!";
+    }
+
+}
